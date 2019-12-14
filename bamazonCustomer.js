2044829b9a85b1a266075c508db6a288fdea1d95
CaoKao <bksaechao@gmail.com>
2019-12-14 00:50:57 -0800

    imported the cli-table package for cleaner display in the terminal and successfuly displayed mock products in the terminal

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,12 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const Table = require("cli-table")
+
+// instantiate
+var table = new Table({
+    head: ['id', 'Name', 'Company', 'Price', 'Stock'],
+    colWidths: [5, 20, 20, 20, 20]
+});
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -15,5 +22,19 @@ const connection = mysql.createConnection({
 connection.connect(err => {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    connection.end();
+    queryAllproducts();
 });
+
+function queryAllproducts() {
+    connection.query("SELECT * FROM products", (err, res) => {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            table.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
+        };
+        console.log('=====================================Bamazon Products======================================')
+        console.log(table.toString());
+        connection.end();
+    });
+}
