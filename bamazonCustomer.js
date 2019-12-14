d3bc1b27cc399fc4b70561da8cf4a74b0678b656
CaoKao <bksaechao@gmail.com>
2019-12-13 23:33:55 -0800

    successfully connected to the mysql database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,19 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "N3wp@55word1424",
+    database: "bamazon_db"
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.end();
+});
